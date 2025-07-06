645ad6b45c7777340ec7db6b6b3eac82f6201d3d
Oliver Gurka <xgurka00@stud.fit.vutbr.cz>
2025-07-06 18:20:32 +0200

    feat(app/tests): create session fixture

--- app/tests/src/fixtures.py
+++ app/tests/src/fixtures.py
@@ -1,6 +1,8 @@
 import pytest
 from fastapi.testclient import TestClient
+from sqlmodel import Session
 
+from app.database import engine
 from app.main import app
 
 
@@ -8,3 +10,10 @@ from app.main import app
 def testclient() -> TestClient:
     """Creates and returns test client"""
     return TestClient(app)
+
+
+@pytest.fixture(scope="function", name="session")
+def session_fixture():
+    """Creates and returns a database session"""
+    with Session(engine) as session:
+        yield session
