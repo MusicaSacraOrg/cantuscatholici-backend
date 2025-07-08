90ca43f9e1a31f352a596fd650b38c699300b0cb
Oliver Gurka <xgurka00@stud.fit.vutbr.cz>
2025-07-08 21:03:41 +0200

    feat(tests): set test environment for each test

--- app/tests/src/fixtures.py
+++ app/tests/src/fixtures.py
@@ -1,7 +1,11 @@
+import os
+
 import pytest
+from dotenv import set_key
 from fastapi.testclient import TestClient
 from sqlmodel import Session
 
+import app.config as config
 from app.database import engine
 from app.main import app
 
@@ -17,3 +21,38 @@ def session_fixture():
     """Creates and returns a database session"""
     with Session(engine) as session:
         yield session
+
+
+@pytest.fixture(scope="function", name="testenv", autouse=True)
+def testenv_fixture(
+    tmp_path,
+):
+    env_file_path = tmp_path / ".env.test"
+    env_file_path.touch(mode=0o600, exist_ok=False)
+
+    static_content_prefix = tmp_path / "static_content"
+    os.makedirs(static_content_prefix)
+
+    # Change static content prefix to tmp dir
+    set_key(
+        dotenv_path=env_file_path,
+        key_to_set="APP_STATIC_CONTENT_PREFIX",
+        value_to_set=str(static_content_prefix),
+    )
+    # Set database logging to always true
+    set_key(
+        dotenv_path=env_file_path,
+        key_to_set="LOG_DB_ENGINE_ECHO",
+        value_to_set="True",
+    )
+
+    config.app_settings = config.AppSettings(
+        _env_file=env_file_path,  # pyright: ignore [reportCallIssue]
+    )
+    config.log_settings = config.LogSettings(
+        _env_file=env_file_path,  # pyright: ignore [reportCallIssue]
+    )
+
+    yield env_file_path
+
+    os.remove(env_file_path)
