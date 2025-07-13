e6bcf9e3b7ed9bd8e8c12e0ce64919ebd173b4d4
Michal Balogh <baloghmichal03@gmail.com>
2025-07-13 15:53:17 +0200

    feat(tag): add Tag model and corresponding test case - simple example model and test case

--- app/tests/tag/test_tag.py
+++ app/tests/tag/test_tag.py
@@ -1,5 +1,22 @@
+
+from app.tag.models import Tag
+
+
 def test_dummy(
     testclient,
 ):
     response = testclient.get("/tag")
     assert response.status_code == 200
+
+
+def test_create_tag(session):
+    tag = Tag(name="Advent")
+
+    session.add(tag)
+    session.commit()
+
+    session.refresh(tag)
+
+    assert tag.id is not None
+    assert tag.name == "Advent"
+    assert isinstance(tag.id, int)
