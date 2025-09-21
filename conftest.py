d8bed2e4542999d3ad75b87e74af97fb80adc48c
Oliver Gurka <oliver.gurka@proton.me>
2025-09-21 15:18:59 +0200

    feat(tests): use alembic schema versioning

--- conftest.py
+++ conftest.py
@@ -1,3 +1,17 @@
+import pytest
+
+import alembic.command
+from alembic.config import Config
+
 pytest_plugins = [
     "app.tests.src.fixtures",
 ]
+
+
+# TODO inefficient, need to implement it with rollback
+@pytest.fixture(scope="function")
+def init_schema():
+    config = Config(toml_file="pyproject.toml")
+    alembic.command.upgrade(config, "head")
+    yield
+    alembic.command.downgrade(config, "base")
