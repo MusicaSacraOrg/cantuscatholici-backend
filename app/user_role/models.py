b1800adf12c09bcb77b4613ee6b069b25f82ff58
Oliver Gurka <oliver.gurka@proton.me>
2025-09-30 16:45:45 +0200

    feat(models): add relationship between user and user role

--- app/user_role/models.py
+++ app/user_role/models.py
@@ -1,10 +1,17 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from typing import TYPE_CHECKING
+
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.database import Base
 
+if TYPE_CHECKING:
+    from app.user.models import User
+
 
 class UserRole(Base):
-    __tablename__ = 'user_roles'
+    __tablename__ = "user_roles"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     role: Mapped[str] = mapped_column(nullable=False, unique=True)
+
+    users: Mapped[list["User"]] = relationship("User", back_populates="role")
